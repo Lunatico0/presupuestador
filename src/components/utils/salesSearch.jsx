eeb348702ea823b542009c28a884409e0843c683
Patricio Pittana <pittanapatricio@gmail.com>
2024-12-28 19:53:48 -0300

    Añade filtros de fecha en la búsqueda de ventas; implementa campos de fecha de inicio y fin en el formulario

--- src/components/utils/salesSearch.jsx
+++ src/components/utils/salesSearch.jsx
@@ -4,7 +4,8 @@ const SalesSearch = ({ sales, clients, products, onFilter }) => {
   const [filters, setFilters] = useState({
     client: "",
     product: "",
-    date: "",
+    startDate: "",
+    endDate: "",
   });
 
   const handleFilterChange = (e) => {
@@ -13,26 +14,32 @@ const SalesSearch = ({ sales, clients, products, onFilter }) => {
   };
 
   useEffect(() => {
-    const { client, product, date } = filters;
+    const { client, product, startDate, endDate } = filters;
     const lowerCaseClient = client.toLowerCase();
     const lowerCaseProduct = product.toLowerCase();
 
     const filtered = sales.filter((sale) => {
+      const saleDate = new Date(sale.date);
       const clientData = clients.find((c) => c._id === sale.client._id) || {};
+
+      // Comparar cliente
       const clientMatch =
         clientData.name?.toLowerCase().includes(lowerCaseClient) ||
         clientData.lastName?.toLowerCase().includes(lowerCaseClient) ||
         clientData.dni?.toString().includes(client) ||
         clientData.email?.toLowerCase().includes(lowerCaseClient);
 
+      // Comparar producto
       const productMatch = sale.products.some((p) => {
         const productData = products.find((prod) => prod._id === p.productId) || {};
         return productData.title?.toLowerCase().includes(lowerCaseProduct);
       });
 
-      const dateMatch = date ? sale.date.includes(date) : true;
+      // Comparar fechas
+      const startDateMatch = startDate ? saleDate >= new Date(startDate) : true;
+      const endDateMatch = endDate ? saleDate <= new Date(endDate) : true;
 
-      return clientMatch && productMatch && dateMatch;
+      return clientMatch && productMatch && startDateMatch && endDateMatch;
     });
 
     onFilter(filtered);
@@ -42,7 +49,7 @@ const SalesSearch = ({ sales, clients, products, onFilter }) => {
     <div className="w-full max-w-4xl mx-auto mb-6">
       <div className="bg-gray-800 rounded-lg shadow-md">
         <h2 className="text-2xl px-4 py-3">Filtros</h2>
-        <form className="bg-gray-800 rounded-lg px-4 py-3 shadow-md space-y-4 md:space-y-0 md:flex md:justify-between md:items-center">
+        <form className="bg-gray-800 rounded-lg px-4 py-3 shadow-md space-y-4 md:space-y-0 md:flex md:justify-between md:items-center gap-1">
           <div className="relative w-full md:w-1/3">
             <input
               type="text"
@@ -77,20 +84,34 @@ const SalesSearch = ({ sales, clients, products, onFilter }) => {
               Producto
             </label>
           </div>
-          <div className="relative w-full md:w-1/3 mt-4 md:mt-0">
+          <div className="relative w-full md:w-1/4 mt-4 md:mt-0">
             <input
               type="date"
-              name="date"
-              placeholder=" "
-              value={filters.date}
+              name="startDate"
+              value={filters.startDate}
               onChange={handleFilterChange}
               className="block w-full border border-gray-300 rounded px-4 pt-4 pb-2 focus:border-blue-500 focus:outline-none bg-gray-700 text-white"
             />
             <label
-              className="absolute left-4 top-0 text-gray-400 text-sm transition-all transform -translate-y-1/2 px-1 rounded-full pr-2"
+              className="absolute left-4 top-0 text-gray-400 text-sm transition-all transform -translate-y-1/2 px-1 bg-gray-800"
               style={{ backdropFilter: 'blur(3px)' }}
             >
-              Fecha
+              Desde
+            </label>
+          </div>
+          <div className="relative w-full md:w-1/4 mt-4 md:mt-0">
+            <input
+              type="date"
+              name="endDate"
+              value={filters.endDate}
+              onChange={handleFilterChange}
+              className="block w-full border border-gray-300 rounded px-4 pt-4 pb-2 focus:border-blue-500 focus:outline-none bg-gray-700 text-white"
+            />
+            <label
+              className="absolute left-4 top-0 text-gray-400 text-sm transition-all transform -translate-y-1/2 px-1 bg-gray-800"
+              style={{ backdropFilter: 'blur(3px)' }}
+            >
+              Hasta
             </label>
           </div>
         </form>
